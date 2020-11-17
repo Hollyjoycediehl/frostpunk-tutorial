230f59521051c1a33023b95d16c3b9c4dd99b99f
Hollyjoycediehl <65977785+Hollyjoycediehl@users.noreply.github.com>
2020-11-16 19:58:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,5 +15,12 @@
     movement = -(topDistance * depth);
   }
 
+ translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+            layer.style['-webkit-transform'] = translate3d;
+            layer.style['-moz-transform'] = translate3d;
+            layer.style['-ms-transform'] = translate3d;
+            layer.style['-o-transform'] = translate3d;
+            layer.style.transform = translate3d;
+
 
 
