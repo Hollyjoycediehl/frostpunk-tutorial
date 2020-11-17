6d8ab6cf44150a6a35017fcbe692e8fa88de0de1
Hollyjoycediehl <65977785+Hollyjoycediehl@users.noreply.github.com>
2020-11-16 19:54:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,12 @@
   });
 
 }).call(this);
+
+  var depth, layer, _i, _len;
+
+  for (_i = 0, _len = layers.length; _i < _len; _i++) {
+    layer = layers[_i];
+    depth = layer.getAttribute(‘data - depth’);
+  }
+
+
