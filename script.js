bb2f9e87758a52550b4b3c0caa58e20793241c11
Hollyjoycediehl <65977785+Hollyjoycediehl@users.noreply.github.com>
2020-11-16 19:59:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 (function() {
-  window.addEventListener(‘scroll’, function(event) {
-    var layers, topDistance;
-    topDistance = this.pageYOffset;
-    return layers = document.querySelectorAll("[data-type='parallax']");
-  });
-
-}).call(this);
-
-  var depth, layer, _i, _len;
-
-  for (_i = 0, _len = layers.length; _i < _len; _i++) {
-    layer = layers[_i];
-    depth = layer.getAttribute(‘data - depth’);
-    movement = -(topDistance * depth);
-  }
-
- translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+    window.addEventListener('scroll', function(event) {
+        var depth, i, layer, layers, len, movement, topDistance, translate3d;
+        topDistance = this.pageYOffset;
+        layers = document.querySelectorAll("[data-type='parallax']");
+        for (i = 0, len = layers.length; i < len; i++) {
+            layer = layers[i];
+            depth = layer.getAttribute('data-depth');
+            movement = -(topDistance * depth);
+            translate3d = 'translate3d(0, ' + movement + 'px, 0)';
             layer.style['-webkit-transform'] = translate3d;
             layer.style['-moz-transform'] = translate3d;
             layer.style['-ms-transform'] = translate3d;
             layer.style['-o-transform'] = translate3d;
             layer.style.transform = translate3d;
-
+        }
+    });
+}).call(this);
 
 
